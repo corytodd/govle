269bfdb4b5aeb9318392672b805b99f975b59851
Cory Todd <cory@ardonyx.io>
2021-11-18 21:41:34 -0700

    make slide function fancier
    Add forward/reverse option
    Add color color option

--- govle/govle.py
+++ govle/govle.py
@@ -1,4 +1,6 @@
-from bleak import BleakClient, BleakScanner
+import asyncio
+
+from bleak import BleakScanner
 
 from govle import color, gov, le
 from govle.logging import logger
@@ -27,37 +29,44 @@ class Govle(object):
 
     async def set_power(self, on: bool) -> None:
         """Set power to on or off state"""
+        logger.debug(f"setting power: {on}")
         packet = gov.Gov().set_power(on)
         await self.le.write(packet)
 
     async def set_brightness(self, level: int) -> None:
         """Set brightness level. Level is in range 0-255."""
+        logger.debug(f"setting brightness: {level}")
         packet = gov.Gov().set_brightness(level)
         await self.le.write(packet)
 
     async def set_gradient(self, on: bool) -> None:
         """Set gradient effect to on or off"""
+        logger.debug(f"setting gradient: {on}")
         packet = gov.Gov().set_gradient(on)
         await self.le.write(packet)
 
     async def set_color(self, rgb) -> None:
         """Set RGB color. Each color is in the range 0-255."""
+        logger.debug(f"setting color: {color.to_hex(rgb)}")
         packet = gov.Gov().set_manual_color(rgb)
         await self.le.write(packet)
 
-    async def slide(self, background=color.WHITE, spot=color.RED) -> None:
+    async def slide(self, background=color.WHITE, spot=color.GREEN, hold=0, forward=True) -> None:
         """Moves one segment around the complete LED strip"""
         await self.set_gradient(True)
 
-        for bit in range(16):
+        bits = range(16) if forward else range(15, -1, -1)
+        for bit in bits:
             selected = 1 << bit
             seg = gov.bitmask_to_segment(selected)
             segment_packet = gov.Gov().set_segment_color(spot, seg)
             await self.le.write(segment_packet)
 
             seg = gov.bitmask_to_segment(~selected)
-            backgroun_packet = gov.Gov().set_segment_color(background, seg)
-            await self.le.write(backgroun_packet)
+            background_packet = gov.Gov().set_segment_color(background, seg)
+            await self.le.write(background_packet)
+
+            await asyncio.sleep(hold)
 
     @staticmethod
     async def discover():
