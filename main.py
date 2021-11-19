2ef6b340a3d17b3cf7fef7d2458c099783f29280
Cory Todd <cory@ardonyx.io>
2021-11-19 08:58:13 -0700

    add diy command to main
    Also reorganize main a bit

--- main.py
+++ main.py
@@ -11,7 +11,23 @@ from config import *
 logging.getLogger('govle').setLevel(logging.DEBUG)
 
 
-async def main(device_name: str, operation: str, brightness: int, rgb: tuple):
+async def main():
+    parser = argparse.ArgumentParser("description='govle LED strip tool'")
+    parser.add_argument("device", help="Which device to control", choices=devices.keys())
+    parser.add_argument('-o', "--operation", help="Operation to perform", default="discover")
+    parser.add_argument('-l', "--level", help="Brightness level, 0-255", type=int)
+    parser.add_argument('-r', "--rgb", nargs=3, type=int)
+    args = parser.parse_args()
+
+    if args.operation == "brightness" and args.level is None:
+        parser.error("brightness operation requires --level parameter")
+    elif args.operation == "color" and args.rgb is None:
+        parser.error("color operation requires --rgb parameter")
+
+    device_name = args.device
+    operation = args.operation
+    level = args.level
+    rgb = args.rgb
 
     if operation == "discover":
         await govle.Govle.discover()
@@ -27,32 +43,19 @@ async def main(device_name: str, operation: str, brightness: int, rgb: tuple):
         elif operation == "gradient-off":
             await gle.set_gradient(False)
         elif operation == "brightness":
-            await gle.set_brightness(brightness)
+            await gle.set_brightness(level)
         elif operation == "color":
             await gle.set_color(rgb)
         elif operation == "slide":
             forward = True
+            back, spot = color.get_random_complementary_pair()
             for _ in range(100):
-                back, spot = color.get_random_complementary_pair()
                 await gle.slide(back, spot, hold=0.1, forward=forward)
                 forward = not forward
+        elif operation == "diy":
+            await gle.diy()
         else:
-            raise RuntimeError(f"Unknown operation: {operation}")
+            parser.error(f"unknown operation '{operation}'")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("description='govle LED strip tool'")
-    parser.add_argument("device", help="Which device to control", choices=devices.keys())
-
-    operation_choices = ("on", "off", "gradient-on", "gradient-off", "color", "brightness", "slide", "discover")
-    parser.add_argument('-o', "--operation", help="Operation to perform", choices=operation_choices, default="on")
-
-    parser.add_argument('-l', "--level", help="Brightness level, 0-255", type=int)
-    parser.add_argument('-r', "--rgb", nargs=3, type=int)
-    args = parser.parse_args()
-
-    if args.operation == "brightness" and args.level is None:
-        parser.error("brightness operation requires --level parameter")
-    elif args.operation == "color" and args.rgb is None:
-        parser.error("color operation requires --rgb parameter")
-
-    asyncio.run(main(args.device, args.operation, args.level, args.rgb))
+    asyncio.run(main())
